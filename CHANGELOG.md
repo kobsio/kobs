c3f028074ce0be070dd08e3f960493ca0fae76af
Rico Berger <ricoberger@users.noreply.github.com>
2021-09-03 22:26:24 +0200

    Support the download of documents (#132)
    It is now possible to download the JSON representation of a log line in
    the ClickHouse and Elasticsearch plugin.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -23,6 +23,7 @@ NOTE: As semantic versioning states all 0.y.z releases can contain breaking chan
 - [#127](https://github.com/kobsio/kobs/pull/127): Allow `ILIKE` queries for ClickHouse logs, using the new `=~` operator.
 - [#128](https://github.com/kobsio/kobs/pull/128): Allow users to specify dashboards within a Team or Application via the new `inline` property.
 - [#131](https://github.com/kobsio/kobs/pull/131): Add chart which shows the distribution of the logs lines in the selected time range for the ClickHouse plugin.
+- [#132](https://github.com/kobsio/kobs/pull/132): Support the download of log lines in their JSON representation in the ClickHouse and Elasticsearch plugin.
 
 ### Fixed
 
--- plugins/clickhouse/src/components/panel/details/Details.tsx
+++ plugins/clickhouse/src/components/panel/details/Details.tsx
@@ -10,11 +10,15 @@ import {
   DrawerHead,
   DrawerPanelBody,
   DrawerPanelContent,
+  Tab,
+  TabTitleText,
+  Tabs,
 } from '@patternfly/react-core';
-import React from 'react';
+import React, { useState } from 'react';
 
+import { Editor, Title } from '@kobsio/plugin-core';
+import Actions from './Actions';
 import { IDocument } from '../../../utils/interfaces';
-import { Title } from '@kobsio/plugin-core';
 import { formatTimeWrapper } from '../../../utils/helpers';
 
 export interface IDetailsProps {
@@ -23,6 +27,8 @@ export interface IDetailsProps {
 }
 
 const Details: React.FunctionComponent<IDetailsProps> = ({ document, close }: IDetailsProps) => {
+  const [activeTab, setActiveTab] = useState<string>('overview');
+
   return (
     <DrawerPanelContent minSize="50%">
       <DrawerHead>
@@ -32,23 +38,43 @@ const Details: React.FunctionComponent<IDetailsProps> = ({ document, close }: ID
           size="lg"
         />
         <DrawerActions style={{ padding: 0 }}>
+          <Actions document={document} />
           <DrawerCloseButton onClose={close} />
         </DrawerActions>
       </DrawerHead>
 
       <DrawerPanelBody>
-        <Card>
-          <CardBody>
-            <DescriptionList className="pf-u-text-break-word">
-              {Object.keys(document).map((key) => (
-                <DescriptionListGroup key={key}>
-                  <DescriptionListTerm>{key}</DescriptionListTerm>
-                  <DescriptionListDescription>{document[key]}</DescriptionListDescription>
-                </DescriptionListGroup>
-              ))}
-            </DescriptionList>
-          </CardBody>
-        </Card>
+        <Tabs
+          activeKey={activeTab}
+          onSelect={(event, tabIndex): void => setActiveTab(tabIndex.toString())}
+          className="pf-u-mt-md"
+          isFilled={true}
+          mountOnEnter={true}
+        >
+          <Tab eventKey="overview" title={<TabTitleText>Overview</TabTitleText>}>
+            <div style={{ maxWidth: '100%', overflowX: 'scroll', padding: '24px 24px' }}>
+              <Card>
+                <CardBody>
+                  <DescriptionList className="pf-u-text-break-word">
+                    {Object.keys(document).map((key) => (
+                      <DescriptionListGroup key={key}>
+                        <DescriptionListTerm>{key}</DescriptionListTerm>
+                        <DescriptionListDescription>{document[key]}</DescriptionListDescription>
+                      </DescriptionListGroup>
+                    ))}
+                  </DescriptionList>
+                </CardBody>
+              </Card>
+            </div>
+          </Tab>
+          <Tab eventKey="json" title={<TabTitleText>JSON</TabTitleText>}>
+            <div style={{ maxWidth: '100%', overflowX: 'scroll', padding: '24px 24px' }}>
+              <Card>
+                <Editor value={JSON.stringify(document, null, 2)} mode="json" readOnly={true} />
+              </Card>
+            </div>
+          </Tab>
+        </Tabs>
       </DrawerPanelBody>
     </DrawerPanelContent>
   );
