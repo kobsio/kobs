9ff525fea0efb75a7596c33ce2a0a26ab81362e0
ricoberger <mail@ricoberger.de>
2021-10-24 16:41:10 +0200

    [clickhouse] Use pagination to display logs
    To display the logs in the table we are now using pagination, instead of
    the Intersection Observer API, to avoid flickering while scrolling
    through the logs.

--- plugins/clickhouse/src/components/panel/LogsDocuments.tsx
+++ plugins/clickhouse/src/components/panel/LogsDocuments.tsx
@@ -3,14 +3,22 @@ import {
   SortByDirection,
   TableComposable,
   TableVariant,
+  Tbody,
+  Td,
   Th,
   Thead,
   Tr,
 } from '@patternfly/react-table';
-import React from 'react';
+import { Pagination, PaginationVariant } from '@patternfly/react-core';
+import React, { useEffect, useState } from 'react';
 
 import { IDocument } from '../../utils/interfaces';
 import LogsDocument from './LogsDocument';
+
+interface IPage {
+  page: number;
+  perPage: number;
+}
 
 interface ILogsDocumentsProps {
   documents?: IDocument[];
@@ -31,8 +39,14 @@ const LogsDocuments: React.FunctionComponent<ILogsDocumentsProps> = ({
   changeOrder,
   selectField,
 }: ILogsDocumentsProps) => {
+  const [page, setPage] = useState<IPage>({ page: 1, perPage: 100 });
+
   const activeSortIndex = fields && orderBy && orderBy !== 'timestamp' ? fields?.indexOf(orderBy) : -1;
   const activeSortDirection = order === 'ascending' ? 'asc' : 'desc';
+
+  useEffect(() => {
+    setPage({ page: 1, perPage: 100 });
+  }, [documents]);
 
   return (
     <TableComposable aria-label="Logs" variant={TableVariant.compact} borders={false}>
@@ -86,16 +100,55 @@ const LogsDocuments: React.FunctionComponent<ILogsDocumentsProps> = ({
         </Tr>
       </Thead>
       {documents
-        ? documents.map((document, index) => (
-            <LogsDocument
-              key={index}
-              document={document}
-              fields={fields}
-              addFilter={addFilter}
-              selectField={selectField}
-            />
-          ))
+        ? documents
+            .slice((page.page - 1) * page.perPage, page.page * page.perPage)
+            .map((document, index) => (
+              <LogsDocument
+                key={index}
+                document={document}
+                fields={fields}
+                addFilter={addFilter}
+                selectField={selectField}
+              />
+            ))
         : null}
+
+      {documents && (
+        <Tbody>
+          <Tr>
+            <Td />
+            <Td colSpan={fields && fields.length > 0 ? fields.length + 1 : 2}>
+              <Pagination
+                itemCount={documents.length}
+                widgetId="pagination-options-menu-bottom"
+                perPage={page.perPage}
+                page={page.page}
+                variant={PaginationVariant.bottom}
+                onSetPage={(event: React.MouseEvent | React.KeyboardEvent | MouseEvent, newPage: number): void =>
+                  setPage({ page: newPage, perPage: page.perPage })
+                }
+                onPerPageSelect={(
+                  event: React.MouseEvent | React.KeyboardEvent | MouseEvent,
+                  newPerPage: number,
+                ): void => setPage({ page: page.page, perPage: newPerPage })}
+                onFirstClick={(event: React.SyntheticEvent<HTMLButtonElement>, newPage: number): void =>
+                  setPage({ page: newPage, perPage: page.perPage })
+                }
+                onLastClick={(event: React.SyntheticEvent<HTMLButtonElement>, newPage: number): void =>
+                  setPage({ page: newPage, perPage: page.perPage })
+                }
+                onNextClick={(event: React.SyntheticEvent<HTMLButtonElement>, newPage: number): void =>
+                  setPage({ page: newPage, perPage: page.perPage })
+                }
+                onPreviousClick={(event: React.SyntheticEvent<HTMLButtonElement>, newPage: number): void =>
+                  setPage({ page: newPage, perPage: page.perPage })
+                }
+              />
+            </Td>
+            <Td />
+          </Tr>
+        </Tbody>
+      )}
     </TableComposable>
   );
 };
