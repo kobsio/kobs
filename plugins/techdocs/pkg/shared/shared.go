24b350968bb0dd0d687f77a30ddb83553949bb03
Rico Berger <ricoberger@users.noreply.github.com>
2021-12-09 18:36:47 +0100

    [techdocs] Ignore comments from code blocks in toc (#227)
    This commit fixes a bug, where the comment from a code block which
    starts with "##" was displayed in the table of contents of a page. We
    are not ignoring these lines now by checking if we are inside a code
    block while looping through the lines of a page.

--- plugins/techdocs/pkg/shared/shared.go
+++ plugins/techdocs/pkg/shared/shared.go
@@ -40,20 +40,30 @@ func ParseIndex(content []byte) (Index, error) {
 // GenerateTOC generates a simple markdown list, which can be used as TOC for the given markdown file.
 func GenerateTOC(markdown string) string {
 	var toc string
+	var isCodeBlock bool
 
 	for _, line := range strings.Split(markdown, "\n") {
 		line = strings.TrimSpace(line)
 
-		if len(line) > 7 && line[0:7] == "###### " {
-			toc += fmt.Sprintf("        - [%s](#%s)\n", strings.Trim(line, "###### "), slugifyRe.ReplaceAllString(strings.ToLower(strings.Trim(line, "###### ")), "-"))
-		} else if len(line) > 6 && line[0:6] == "##### " {
-			toc += fmt.Sprintf("      - [%s](#%s)\n", strings.Trim(line, "##### "), slugifyRe.ReplaceAllString(strings.ToLower(strings.Trim(line, "##### ")), "-"))
-		} else if len(line) > 5 && line[0:5] == "#### " {
-			toc += fmt.Sprintf("    - [%s](#%s)\n", strings.Trim(line, "#### "), slugifyRe.ReplaceAllString(strings.ToLower(strings.Trim(line, "#### ")), "-"))
-		} else if len(line) > 4 && line[0:4] == "### " {
-			toc += fmt.Sprintf("  - [%s](#%s)\n", strings.Trim(line, "### "), slugifyRe.ReplaceAllString(strings.ToLower(strings.Trim(line, "### ")), "-"))
-		} else if len(line) > 3 && line[0:3] == "## " {
-			toc += fmt.Sprintf("- [%s](#%s)\n", strings.Trim(line, "## "), slugifyRe.ReplaceAllString(strings.ToLower(strings.Trim(line, "## ")), "-"))
+		// We check if the line starts with ``` for a comment block. If this is the case we are setting the isCodeBlock
+		// variable to true and when the code block ends to false. This is required so that we do not add a comment from
+		// a code block to the table of contents.
+		if strings.HasPrefix(line, "```") {
+			isCodeBlock = !isCodeBlock
+		}
+
+		if !isCodeBlock {
+			if len(line) > 7 && line[0:7] == "###### " {
+				toc += fmt.Sprintf("        - [%s](#%s)\n", strings.Trim(line, "###### "), slugifyRe.ReplaceAllString(strings.ToLower(strings.Trim(line, "###### ")), "-"))
+			} else if len(line) > 6 && line[0:6] == "##### " {
+				toc += fmt.Sprintf("      - [%s](#%s)\n", strings.Trim(line, "##### "), slugifyRe.ReplaceAllString(strings.ToLower(strings.Trim(line, "##### ")), "-"))
+			} else if len(line) > 5 && line[0:5] == "#### " {
+				toc += fmt.Sprintf("    - [%s](#%s)\n", strings.Trim(line, "#### "), slugifyRe.ReplaceAllString(strings.ToLower(strings.Trim(line, "#### ")), "-"))
+			} else if len(line) > 4 && line[0:4] == "### " {
+				toc += fmt.Sprintf("  - [%s](#%s)\n", strings.Trim(line, "### "), slugifyRe.ReplaceAllString(strings.ToLower(strings.Trim(line, "### ")), "-"))
+			} else if len(line) > 3 && line[0:3] == "## " {
+				toc += fmt.Sprintf("- [%s](#%s)\n", strings.Trim(line, "## "), slugifyRe.ReplaceAllString(strings.ToLower(strings.Trim(line, "## ")), "-"))
+			}
 		}
 	}
 
