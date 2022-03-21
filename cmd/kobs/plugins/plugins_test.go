4dd56d7e15f2206f832cb5dedf12b48ddc0e4457
Axel Koehler <axel@staffbase.com>
2022-03-21 08:23:56 +0100

    :green_heart: [backstage] Fix test

--- cmd/kobs/plugins/plugins_test.go
+++ cmd/kobs/plugins/plugins_test.go
@@ -1,6 +1,9 @@
 package plugins
 
 import (
+	"github.com/kobsio/kobs/plugins/backstage"
+	"github.com/kobsio/kobs/plugins/prometheus"
+	"github.com/kobsio/kobs/plugins/prometheus/pkg/instance"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -23,6 +26,13 @@ func TestGetPlugins(t *testing.T) {
 				Type:        "applications",
 			},
 			{
+				Name:        "prometheus",
+				DisplayName: "Prometheus",
+				Description: "From metrics to insight: Power your metrics and alerting with a leading open-source monitoring solution.",
+				Home:        true,
+				Type:        "prometheus",
+			},
+			{
 				Name:        "resources",
 				DisplayName: "Resources",
 				Description: "View and edit Kubernetes resources.",
@@ -38,10 +48,20 @@ func TestGetPlugins(t *testing.T) {
 	router.getPlugins(w, req)
 
 	require.Equal(t, http.StatusOK, w.Code)
-	require.Equal(t, "[{\"name\":\"applications\",\"displayName\":\"Applications\",\"description\":\"Monitor your Kubernetes workloads.\",\"home\":true,\"type\":\"applications\",\"options\":null},{\"name\":\"resources\",\"displayName\":\"Resources\",\"description\":\"View and edit Kubernetes resources.\",\"home\":false,\"type\":\"resources\",\"options\":null}]\n", string(w.Body.Bytes()))
+	require.Equal(t, "[{\"name\":\"applications\",\"displayName\":\"Applications\",\"description\":\"Monitor your Kubernetes workloads.\",\"home\":true,\"type\":\"applications\",\"options\":null},{\"name\":\"prometheus\",\"displayName\":\"Prometheus\",\"description\":\"From metrics to insight: Power your metrics and alerting with a leading open-source monitoring solution.\",\"home\":true,\"type\":\"prometheus\",\"options\":null},{\"name\":\"resources\",\"displayName\":\"Resources\",\"description\":\"View and edit Kubernetes resources.\",\"home\":false,\"type\":\"resources\",\"options\":null}]\n", string(w.Body.Bytes()))
 }
 
 func TestRegister(t *testing.T) {
-	router := Register(nil, Config{})
+	cfg := Config{
+		Backstage: backstage.Config{
+			PrometheusName: "prometheus",
+		},
+		Prometheus: prometheus.Config{instance.Config{
+			Name:        "prometheus",
+			DisplayName: "Prometheus",
+			Description: "From metrics to insight: Power your metrics and alerting with a leading open-source monitoring solution.",
+		}},
+	}
+	router := Register(nil, cfg)
 	require.NotEmpty(t, router)
 }
