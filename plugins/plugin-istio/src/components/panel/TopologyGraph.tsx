c4589c0973dd9a183b712210acca99813f6782d9
ricoberger <mail@ricoberger.de>
2022-06-10 15:30:04 +0200

    Fix cytoscape graphs

--- plugins/plugin-istio/src/components/panel/TopologyGraph.tsx
+++ plugins/plugin-istio/src/components/panel/TopologyGraph.tsx
@@ -1,66 +1,26 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import CytoscapeComponent from 'react-cytoscapejs';
+import React, { useCallback, useEffect, useRef } from 'react';
 import cytoscape from 'cytoscape';
 import dagre from 'cytoscape-dagre';
 import nodeHtmlLabel from 'cytoscape-node-html-label';
 
 import { IEdge, INode, INodeData } from '../../utils/interfaces';
-import { IPluginInstance, ITimes } from '@kobsio/shared';
+import { IPluginInstance, ITimes, useDimensions } from '@kobsio/shared';
 import DetailsMetrics from './details/DetailsMetrics';
 import { formatNumber } from '../../utils/helpers';
 
 import '../../assets/topologygraph.css';
 
-cytoscape.use(dagre);
-nodeHtmlLabel(cytoscape);
-
-// layout is the layout for the topology graph.
+// dagreLayout is the layout for the topology graph.
 // See: https://js.cytoscape.org/#layouts
-const layout = {
+const dagreLayout = {
   fit: true,
   name: 'dagre',
   nodeDimensionsIncludeLabels: true,
   rankDir: 'LR',
 };
 
-// styleSheet changes the style of the nodes and edges in the topology graph.
-// See: https://js.cytoscape.org/#style
-const styleSheet: cytoscape.Stylesheet[] = [
-  {
-    selector: 'node',
-    style: {
-      'background-color': '#ffffff',
-      'border-color': '#6a6e73',
-      'border-opacity': 1,
-      'border-width': 1,
-      color: '#151515',
-      'font-family': 'RedHatDisplay, Overpass, overpass, helvetica, arial, sans-serif',
-      height: '300px',
-      label: 'data(id)',
-      shape: 'roundrectangle',
-      'text-halign': 'center',
-      'text-opacity': 0,
-      'text-valign': 'bottom',
-      'text-wrap': 'wrap',
-      width: '200px',
-    },
-  },
-  {
-    selector: 'edge',
-    style: {
-      'curve-style': 'bezier',
-      'line-color': '#0066cc',
-      'target-arrow-color': '#0066cc',
-      'target-arrow-shape': 'triangle',
-      width: 3,
-    },
-  },
-];
-
 const nodeLabel = (node: INodeData): string => {
-  console.log(node);
-
   const application = node.metrics.hasOwnProperty('destination_app')
     ? node.metrics['destination_app']
     : node.metrics.hasOwnProperty('source_workload')
@@ -128,6 +88,40 @@ const nodeLabel = (node: INodeData): string => {
     </div>`;
 };
 
+// styleSheet changes the style of the nodes and edges in the topology graph.
+// See: https://js.cytoscape.org/#style
+const styleSheet: cytoscape.Stylesheet[] = [
+  {
+    selector: 'node',
+    style: {
+      'background-color': '#ffffff',
+      'border-color': '#6a6e73',
+      'border-opacity': 1,
+      'border-width': 1,
+      color: '#151515',
+      'font-family': 'RedHatDisplay, Overpass, overpass, helvetica, arial, sans-serif',
+      height: '300px',
+      label: 'data(id)',
+      shape: 'roundrectangle',
+      'text-halign': 'center',
+      'text-opacity': 0,
+      'text-valign': 'bottom',
+      'text-wrap': 'wrap',
+      width: '200px',
+    },
+  },
+  {
+    selector: 'edge',
+    style: {
+      'curve-style': 'bezier',
+      'line-color': '#0066cc',
+      'target-arrow-color': '#0066cc',
+      'target-arrow-shape': 'triangle',
+      width: 3,
+    },
+  },
+];
+
 interface ITopologyGraphProps {
   instance: IPluginInstance;
   edges: IEdge[];
@@ -147,13 +141,12 @@ const TopologyGraph: React.FunctionComponent<ITopologyGraphProps> = ({
   times,
   setDetails,
 }: ITopologyGraphProps) => {
-  const [width, setWidth] = useState<number>(0);
-  const [height, setHeight] = useState<number>(0);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const graphRef = useRef<cytoscape.Core | null>(null);
+  const wrapper = useRef<HTMLDivElement>(null);
+  const container = useRef<HTMLDivElement>(null);
+  const graph = useRef<cytoscape.Core>();
+  const layout = useRef<cytoscape.Layouts>();
+  const size = useDimensions(wrapper);
 
-  // onTap is called, when the user selects an edge or node in the topology chart. When the user selects a node we are
-  // showing some details for this node.
   const onTap = useCallback(
     (event: cytoscape.EventObject): void => {
       const node = event.target;
@@ -175,59 +168,71 @@ const TopologyGraph: React.FunctionComponent<ITopologyGraphProps> = ({
     [instance, namespace, application, times, setDetails],
   );
 
-  const cyCallback = useCallback(
-    (cy: cytoscape.Core): void => {
-      if (graphRef.current) return;
-      graphRef.current = cy;
-      cy.on('tap', onTap);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (cy as any).nodeHtmlLabel([
-        {
-          halign: 'center',
-          halignBox: 'center',
-          query: 'node:visible',
-          tpl: nodeLabel,
-          valign: 'center',
-          valignBox: 'center',
-        },
-      ]);
-    },
-    [onTap],
-  );
-
-  // useEffect is used to adjust the size of the topology graph container, so it takes the maximum available space.
   useEffect(() => {
-    if (containerRef && containerRef.current) {
-      setWidth(containerRef.current.getBoundingClientRect().width);
-      setHeight(containerRef.current.getBoundingClientRect().height);
-    }
-  }, [edges, nodes]);
+    if (graph.current) {
+      if (layout.current) {
+        layout.current.stop();
+      }
 
-  // useEffect is used to remove the tab listener, when the component is unmounted.
+      graph.current.add({
+        edges: edges,
+        nodes: nodes,
+      });
+
+      layout.current = graph.current.elements().makeLayout(dagreLayout);
+      layout.current.run();
+    }
+  }, [edges, nodes, size]);
+
+  useEffect(() => {
+    if (!container.current) {
+      return;
+    }
+
+    try {
+      if (!graph.current) {
+        cytoscape.use(dagre);
+        nodeHtmlLabel(cytoscape);
+
+        graph.current = cytoscape({
+          container: container.current,
+          elements: {
+            edges: edges,
+            nodes: nodes,
+          },
+          style: styleSheet,
+        });
+        graph.current.on('tap', onTap);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (graph.current as any).nodeHtmlLabel([
+          {
+            halign: 'center',
+            halignBox: 'center',
+            query: 'node:visible',
+            tpl: nodeLabel,
+            valign: 'center',
+            valignBox: 'center',
+          },
+        ]);
+      }
+    } catch (err) {}
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [size]);
+
   useEffect(() => {
     return (): void => {
-      if (graphRef.current) {
-        graphRef.current.removeListener('tab', onTap);
-        graphRef.current = null;
+      if (graph.current) {
+        graph.current.removeListener('tab', onTap);
+        graph.current.destroy();
       }
     };
-  }, [onTap]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <div style={{ minHeight: '100%' }} ref={containerRef}>
-      {height > 0 && width > 0 ? (
-        <CytoscapeComponent
-          elements={CytoscapeComponent.normalizeElements({ edges: edges, nodes: nodes })}
-          style={{ height: `${height}px`, width: `${width}px` }}
-          zoomingEnabled={true}
-          maxZoom={5}
-          minZoom={0.1}
-          autounselectify={false}
-          boxSelectionEnabled={true}
-          layout={layout}
-          stylesheet={styleSheet}
-          cy={cyCallback}
-        />
+    <div style={{ minHeight: '100%' }} ref={wrapper}>
+      {size.height > 0 && size.width > 0 ? (
+        <div style={{ height: `${size.height}px`, width: `${size.width}px` }} ref={container} />
       ) : null}
     </div>
   );
