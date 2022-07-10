kobsio/kobs

commit 8edfb2f741071ec9b87b4d7668a5168957b65a26

[app] Add Notifications (#389)
It is now possible to show notifications within kobs. Notifications can
be configured using the hub configuration file via plugins. When
notifications are configured a notification bell icon is added to the
header in the kobs ui. When a user clicks on the icon a notification
drawer is shown with all the retrieved notification items from a plugin.

In the this first iteration only the Opsgenie plugin is supported for
notifications to show open alerts and incidents.

For later is is planed to also allow resources and maybe also RSS feeds
to be displayed in the notification section.

pkg/hub/hub.go
@@ -10,6 +10,7 @@ import (
 	"github.com/kobsio/kobs/pkg/hub/api/clusters"
 	"github.com/kobsio/kobs/pkg/hub/api/dashboards"
 	"github.com/kobsio/kobs/pkg/hub/api/navigation"
+	"github.com/kobsio/kobs/pkg/hub/api/notifications"
 	"github.com/kobsio/kobs/pkg/hub/api/plugins"
 	"github.com/kobsio/kobs/pkg/hub/api/resources"
 	"github.com/kobsio/kobs/pkg/hub/api/teams"
@@ -98,6 +99,7 @@ func New(config api.Config, debugUsername, debugPassword, hubAddress string, aut
 
 		r.Mount("/auth", userauth.Mount(authLogoutRedirect))
 		r.Mount("/navigation", navigation.Mount(config.Navigation))
+		r.Mount("/notifications", notifications.Mount(config.Notifications))
 		r.Mount("/clusters", clusters.Mount(storeClient))
 		r.Mount("/applications", applications.Mount(storeClient))
 		r.Mount("/teams", teams.Mount(storeClient))

mkdocs.yml
@@ -42,6 +42,7 @@ nav:
           - satellite: getting-started/configuration/satellite.md
           - Authentication: getting-started/configuration/authentication.md
           - Navigation: getting-started/configuration/navigation.md
+          - Notifications: getting-started/configuration/notifications.md
       - Demo: getting-started/demo/demo.md
   - Resources:
       - resources/index.md

pkg/hub/api/api.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"github.com/kobsio/kobs/pkg/hub/api/navigation"
+	"github.com/kobsio/kobs/pkg/hub/api/notifications"
 	"github.com/kobsio/kobs/pkg/hub/api/users"
 )
 
 type Config struct {
-	Users      users.Config      `json:"users"`
-	Navigation navigation.Config `json:"navigation"`
+	Users         users.Config         `json:"users"`
+	Navigation    navigation.Config    `json:"navigation"`
+	Notifications notifications.Config `json:"notifications"`
 }

pkg/hub/api/notifications/notifications.go
@@ -1,0 +1,39 @@
+package notifications
+
+import (
+	"net/http"
+
+	dashboardv1 "github.com/kobsio/kobs/pkg/kube/apis/dashboard/v1"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+)
+
+type Config struct {
+	Groups []group `json:"groups"`
+}
+
+type group struct {
+	Title  string             `json:"title"`
+	Plugin dashboardv1.Plugin `json:"plugin"`
+}
+
+type Router struct {
+	*chi.Mux
+	config Config
+}
+
+func (router *Router) getNotificationGroups(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, router.config.Groups)
+}
+
+func Mount(config Config) chi.Router {
+	router := Router{
+		chi.NewRouter(),
+		config,
+	}
+
+	router.Get("/groups", router.getNotificationGroups)
+
+	return router
+}

pkg/hub/api/notifications/notifications_test.go
@@ -1,0 +1,53 @@
+package notifications
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	dashboardv1 "github.com/kobsio/kobs/pkg/kube/apis/dashboard/v1"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetNotificationGroups(t *testing.T) {
+	for _, tt := range []struct {
+		name               string
+		config             Config
+		expectedStatusCode int
+		expectedBody       string
+	}{
+		{
+			name:               "not configured",
+			expectedStatusCode: http.StatusOK,
+			expectedBody:       "null\n",
+		},
+		{
+			name:               "configured",
+			config:             Config{Groups: []group{{Title: "Alerts", Plugin: dashboardv1.Plugin{Name: "opsgenie", Type: "opsgenie"}}}},
+			expectedStatusCode: http.StatusOK,
+			expectedBody:       "[{\"title\":\"Alerts\",\"plugin\":{\"type\":\"opsgenie\",\"name\":\"opsgenie\"}}]\n",
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			router := Router{chi.NewRouter(), tt.config}
+
+			req, _ := http.NewRequest(http.MethodGet, "/groups", nil)
+			rctx := chi.NewRouteContext()
+			req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+
+			w := httptest.NewRecorder()
+			router.getNotificationGroups(w, req)
+
+			require.Equal(t, tt.expectedStatusCode, w.Code)
+			require.Equal(t, tt.expectedBody, string(w.Body.Bytes()))
+		})
+	}
+}
+
+func TestMount(t *testing.T) {
+	router := Mount(Config{Groups: []group{}})
+	require.NotNil(t, router)
+}

plugins/app/src/App.tsx
@@ -5,12 +5,14 @@ import '@patternfly/patternfly/patternfly-addons.css';
 import { Alert, AlertVariant, Page, Spinner } from '@patternfly/react-core';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import ReactDOM from 'react-dom/client';
 
 import { AuthContextProvider } from './context/AuthContext';
 import Header from './components/header/Header';
+import Notifications from './components/header/Notifications';
+import { NotificationsContextProvider } from './context/NotificationsContext';
 import { PluginsContextProvider } from './context/PluginsContext';
 
 import 'xterm/css/xterm.css';
@@ -42,55 +44,75 @@ const queryClient = new QueryClient({
 });
 
 const App: React.FunctionComponent = () => {
+  const [isNotificationDrawerExpanded, setIsNotificationDrawerExpanded] = useState<boolean>(false);
+
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <PluginsContextProvider>
-          <BrowserRouter>
-            <Page isManagedSidebar={true} header={<Header />} sidebar={<Sidebar />}>
-              <ErrorBoundary
-                fallbackRender={({ error }): React.ReactElement => (
-                  <Alert
-                    style={{ left: '50%', position: 'fixed', top: '50%', transform: 'translate(-50%, -50%)' }}
-                    variant={AlertVariant.danger}
-                    title="An error occured"
-                  >
-                    <p>{error?.message}</p>
-                  </Alert>
-                )}
+        <NotificationsContextProvider>
+          <PluginsContextProvider>
+            <BrowserRouter>
+              <Page
+                isManagedSidebar={true}
+                header={
+                  <Header
+                    isNotificationDrawerExpanded={isNotificationDrawerExpanded}
+                    setIsNotificationDrawerExpanded={setIsNotificationDrawerExpanded}
+                  />
+                }
+                sidebar={<Sidebar />}
+                notificationDrawer={
+                  <Notifications
+                    isNotificationDrawerExpanded={isNotificationDrawerExpanded}
+                    setIsNotificationDrawerExpanded={setIsNotificationDrawerExpanded}
+                  />
+                }
+                isNotificationDrawerExpanded={isNotificationDrawerExpanded}
               >
-                <Suspense
-                  fallback={
-                    <Spinner
+                <ErrorBoundary
+                  fallbackRender={({ error }): React.ReactElement => (
+                    <Alert
                       style={{ left: '50%', position: 'fixed', top: '50%', transform: 'translate(-50%, -50%)' }}
-                    />
-                  }
+                      variant={AlertVariant.danger}
+                      title="An error occured"
+                    >
+                      <p>{error?.message}</p>
+                    </Alert>
+                  )}
                 >
-                  <Routes>
-                    <Route path="/" element={<Navigate to="/applications" replace={true} />} />
-                    <Route path="/applications" element={<Applications />} />
-                    <Route
-                      path="/applications/satellite/:satellite/cluster/:cluster/namespace/:namespace/name/:name"
-                      element={<Application />}
-                    />
-                    <Route
-                      path="/dashboards/satellite/:satellite/cluster/:cluster/namespace/:namespace/name/:name"
-                      element={<DashboardPage />}
-                    />
-                    <Route path="/topology" element={<ApplicationsTopology />} />
-                    <Route path="/teams" element={<Teams />} />
-                    <Route path="/teams/:team" element={<Team />} />
-                    <Route path="/resources" element={<Resources />} />
-                    <Route path="/plugins" element={<PluginInstances />} />
-                    <Route path="/plugins/:satellite/:type/:name/*" element={<PluginPage />} />
-                    <Route path="/profile" element={<Profile />} />
-                    <Route path="/settings" element={<Settings />} />
-                  </Routes>
-                </Suspense>
-              </ErrorBoundary>
-            </Page>
-          </BrowserRouter>
-        </PluginsContextProvider>
+                  <Suspense
+                    fallback={
+                      <Spinner
+                        style={{ left: '50%', position: 'fixed', top: '50%', transform: 'translate(-50%, -50%)' }}
+                      />
+                    }
+                  >
+                    <Routes>
+                      <Route path="/" element={<Navigate to="/applications" replace={true} />} />
+                      <Route path="/applications" element={<Applications />} />
+                      <Route
+                        path="/applications/satellite/:satellite/cluster/:cluster/namespace/:namespace/name/:name"
+                        element={<Application />}
+                      />
+                      <Route
+                        path="/dashboards/satellite/:satellite/cluster/:cluster/namespace/:namespace/name/:name"
+                        element={<DashboardPage />}
+                      />
+                      <Route path="/topology" element={<ApplicationsTopology />} />
+                      <Route path="/teams" element={<Teams />} />
+                      <Route path="/teams/:team" element={<Team />} />
+                      <Route path="/resources" element={<Resources />} />
+                      <Route path="/plugins" element={<PluginInstances />} />
+                      <Route path="/plugins/:satellite/:type/:name/*" element={<PluginPage />} />
+                      <Route path="/profile" element={<Profile />} />
+                      <Route path="/settings" element={<Settings />} />
+                    </Routes>
+                  </Suspense>
+                </ErrorBoundary>
+              </Page>
+            </BrowserRouter>
+          </PluginsContextProvider>
+        </NotificationsContextProvider>
       </AuthContextProvider>
     </QueryClientProvider>
   );

plugins/app/src/components/header/Header.tsx
@@ -14,7 +14,15 @@ import HeaderToolbar from './HeaderToolbar';
 
 import logo from '../../assets/logo.png';
 
-const Header: React.FunctionComponent = () => {
+interface IHeaderProps {
+  isNotificationDrawerExpanded: boolean;
+  setIsNotificationDrawerExpanded: (value: boolean) => void;
+}
+
+const Header: React.FunctionComponent<IHeaderProps> = ({
+  isNotificationDrawerExpanded,
+  setIsNotificationDrawerExpanded,
+}: IHeaderProps) => {
   return (
     <Masthead>
       <MastheadToggle>
@@ -28,7 +36,10 @@ const Header: React.FunctionComponent = () => {
         </MastheadBrand>
       </MastheadMain>
       <MastheadContent>
-        <HeaderToolbar />
+        <HeaderToolbar
+          isNotificationDrawerExpanded={isNotificationDrawerExpanded}
+          setIsNotificationDrawerExpanded={setIsNotificationDrawerExpanded}
+        />
       </MastheadContent>
     </Masthead>
   );

commit 6d54578a615bcf977732ca3ad482ca84638ed104

[app] Improve instrumentation middleware
We unified the exported function name across the httpmetrics, httplog
and httptracer middleware. They all export a Handler function which can
be used in the router.

The logging and tracing middleware also catchs panics now to write a log
line / create a span.

Besides that we also fixed a small bug caused by the notification
drawer, introduced in #389. Because of the notification drawer the
drawer in the PageSectionContent component wasn't using it's full height
anymore, which caused problems for the select boxes in the toolbar and
the pages showing graphs via cytoscape. This should be fixed now.

pkg/hub/hub.go
@@ -21,8 +21,8 @@ import (
 	"github.com/kobsio/kobs/pkg/log"
 	"github.com/kobsio/kobs/pkg/middleware/debug"
 	"github.com/kobsio/kobs/pkg/middleware/httplog"
+	"github.com/kobsio/kobs/pkg/middleware/httpmetrics"
 	"github.com/kobsio/kobs/pkg/middleware/httptracer"
-	"github.com/kobsio/kobs/pkg/middleware/metrics"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -93,8 +93,8 @@ func New(config api.Config, debugUsername, debugPassword, hubAddress string, aut
 		r.Use(middleware.URLFormat)
 		r.Use(userauth.Handler(authEnabled, authHeaderUser, authHeaderTeams, authSessionToken, authSessionInterval, storeClient))
 		r.Use(httptracer.Handler("hub"))
-		r.Use(metrics.Metrics)
-		r.Use(httplog.Logger)
+		r.Use(httpmetrics.Handler)
+		r.Use(httplog.Handler)
 		r.Use(render.SetContentType(render.ContentTypeJSON))
 
 		r.Mount("/auth", userauth.Mount(authLogoutRedirect))

pkg/middleware/httplog/httplog.go
@@ -13,16 +13,41 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-// Logger is a middleware that handles the request logging for chi via zap.
-func Logger(next http.Handler) http.Handler {
+// Handler is a middleware that handles the request logging for chi via zap.
+func Handler(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		wrw := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
-		next.ServeHTTP(wrw, r)
 
 		scheme := "http"
 		if r.TLS != nil {
 			scheme = "https"
+		}
+
+		defer func() {
+			if err := recover(); err != nil {
+				fields := []zapcore.Field{
+					zap.String("requestScheme", scheme),
+					zap.String("requestProto", r.Proto),
+					zap.String("requestMethod", r.Method),
+					zap.String("requestAddr", r.RemoteAddr),
+					zap.String("requestUserAgent", r.UserAgent()),
+					zap.String("requestURI", fmt.Sprintf("%s://%s%s", scheme, r.Host, r.RequestURI)),
+					zap.Int("responseStatus", 500),
+					zap.Float64("requestLatency", float64(time.Since(start).Nanoseconds())/1000000),
+					zap.String("error", fmt.Sprintf("%v", err)),
+				}
+
+				log.Panic(r.Context(), "Request completed", fields...)
+				panic(err)
+			}
+		}()
+
+		wrw := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		next.ServeHTTP(wrw, r)
+
+		status := wrw.Status()
+		if status == 0 {
+			status = http.StatusOK
 		}
 
 		fields := []zapcore.Field{
@@ -32,12 +57,16 @@ func Logger(next http.Handler) http.Handler {
 			zap.String("requestAddr", r.RemoteAddr),
 			zap.String("requestUserAgent", r.UserAgent()),
 			zap.String("requestURI", fmt.Sprintf("%s://%s%s", scheme, r.Host, r.RequestURI)),
-			zap.Int("responseStatus", wrw.Status()),
+			zap.Int("responseStatus", status),
 			zap.Int("responseBytes", wrw.BytesWritten()),
 			zap.Float64("requestLatency", float64(time.Since(start).Nanoseconds())/1000000),
 		}
 
-		log.Info(r.Context(), "Request completed", fields...)
+		if status >= 500 {
+			log.Error(r.Context(), "Request completed", fields...)
+		} else {
+			log.Info(r.Context(), "Request completed", fields...)
+		}
 	}
 
 	return http.HandlerFunc(fn)

pkg/middleware/httpmetrics/httpmetrics.go
@@ -1,4 +1,4 @@
-package metrics
+package httpmetrics
 
 import (
 	"net/http"
@@ -27,8 +27,8 @@ var (
 	}, []string{"response_code", "request_method", "request_path"})
 )
 
-// Metrics is a middleware that handles the Prometheus metrics for kobs and chi.
-func Metrics(next http.Handler) http.Handler {
+// Handler is a middleware that handles the Prometheus metrics for kobs and chi.
+func Handler(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		wrw := middleware.NewWrapResponseWriter(w, r.ProtoMajor)

pkg/middleware/httptracer/httptracer.go
@@ -3,6 +3,7 @@ package httptracer
 import (
 	"fmt"
 	"net/http"
+	"runtime/debug"
 	"strings"
 
 	authContext "github.com/kobsio/kobs/pkg/hub/middleware/userauth/context"
@@ -43,30 +44,49 @@ func (tw traceware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx, span := tw.tracer.Start(ctx, "http.request", oteltrace.WithSpanKind(oteltrace.SpanKindServer))
 	defer span.End()
 
+	defer func() {
+		// In go-chi/chi, full route pattern could only be extracted once the request is executed
+		// See: https://github.com/go-chi/chi/issues/150#issuecomment-278850733
+		routeStr := strings.Join(chi.RouteContext(r.Context()).RoutePatterns, "")
+		span.SetAttributes(semconv.HTTPServerAttributesFromHTTPRequest(tw.serverName, routeStr, r)...)
+		span.SetAttributes(semconv.NetAttributesFromHTTPRequest("tcp", r)...)
+		span.SetAttributes(semconv.EndUserAttributesFromHTTPRequest(r)...)
+		span.SetAttributes(otelhttp.ReadBytesKey.Int64(r.ContentLength))
+
+		if requestID := middleware.GetReqID(ctx); requestID != "" {
+			span.SetAttributes(attribute.Key("request.id").String(requestID))
+		}
+
+		if user, _ := authContext.GetUser(ctx); user != nil {
+			span.SetAttributes(attribute.Key("user.email").String(user.Email))
+		}
+
+		span.SetName(fmt.Sprintf("%s:%s", r.Method, routeStr))
+
+		if err := recover(); err != nil {
+			span.SetAttributes(semconv.HTTPAttributesFromHTTPStatusCode(500)...)
+			span.SetStatus(semconv.SpanStatusFromHTTPStatusCode(500))
+			span.AddEvent("panic", oteltrace.WithAttributes(
+				attribute.String("kind", "panic"),
+				attribute.String("message", fmt.Sprintf("%v", err)),
+				attribute.String("stack", string(debug.Stack())),
+			))
+			span.End()
+
+			panic(err)
+		}
+	}()
+
 	r = r.WithContext(ctx)
 	wrw := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
 	tw.handler.ServeHTTP(wrw, r)
 
-	// In go-chi/chi, full route pattern could only be extracted once the request is executed
-	// See: https://github.com/go-chi/chi/issues/150#issuecomment-278850733
-	routeStr := strings.Join(chi.RouteContext(r.Context()).RoutePatterns, "")
-	attrs := semconv.HTTPAttributesFromHTTPStatusCode(wrw.Status())
-	attrs = append(attrs, semconv.NetAttributesFromHTTPRequest("tcp", r)...)
-	attrs = append(attrs, semconv.EndUserAttributesFromHTTPRequest(r)...)
-	attrs = append(attrs, semconv.HTTPServerAttributesFromHTTPRequest(tw.serverName, routeStr, r)...)
-	attrs = append(attrs, otelhttp.WroteBytesKey.Int(wrw.BytesWritten()))
-	attrs = append(attrs, otelhttp.ReadBytesKey.Int64(r.ContentLength))
-
-	if requestID := middleware.GetReqID(ctx); requestID != "" {
-		attrs = append(attrs, attribute.Key("request.id").String(requestID))
-	}
-	if user, _ := authContext.GetUser(ctx); user != nil {
-		attrs = append(attrs, attribute.Key("user.email").String(user.Email))
+	status := wrw.Status()
+	if status == 0 {
+		status = http.StatusOK
 	}
 
-	span.SetAttributes(attrs...)
-	span.SetName(fmt.Sprintf("%s:%s", r.Method, routeStr))
-
-	spanStatus, spanMessage := semconv.SpanStatusFromHTTPStatusCode(wrw.Status())
-	span.SetStatus(spanStatus, spanMessage)
+	span.SetAttributes(otelhttp.WroteBytesKey.Int(wrw.BytesWritten()))
+	span.SetAttributes(semconv.HTTPAttributesFromHTTPStatusCode(status)...)
+	span.SetStatus(semconv.SpanStatusFromHTTPStatusCode(status))
 }

pkg/satellite/satellite.go
@@ -9,8 +9,8 @@ import (
 	"github.com/kobsio/kobs/pkg/log"
 	"github.com/kobsio/kobs/pkg/middleware/debug"
 	"github.com/kobsio/kobs/pkg/middleware/httplog"
+	"github.com/kobsio/kobs/pkg/middleware/httpmetrics"
 	"github.com/kobsio/kobs/pkg/middleware/httptracer"
-	"github.com/kobsio/kobs/pkg/middleware/metrics"
 	"github.com/kobsio/kobs/pkg/satellite/api"
 	"github.com/kobsio/kobs/pkg/satellite/api/applications"
 	apiClusters "github.com/kobsio/kobs/pkg/satellite/api/clusters"
@@ -93,8 +93,8 @@ func New(debugUsername, debugPassword, satelliteAddress, satelliteToken string, 
 		r.Use(tokenauth.Handler(satelliteToken))
 		r.Use(user.Handler())
 		r.Use(httptracer.Handler("satellite"))
-		r.Use(metrics.Metrics)
-		r.Use(httplog.Logger)
+		r.Use(httpmetrics.Handler)
+		r.Use(httplog.Handler)
 		r.Use(render.SetContentType(render.ContentTypeJSON))
 
 		r.Mount("/clusters", apiClusters.Mount(apiConfig.Clusters, clustersClient))

plugins/app/src/assets/index.css
@@ -16,3 +16,16 @@ html, body, #root {
 .kobsio-hide-scrollbar::-webkit-scrollbar {
   display: none;
 }
+
+/* .pf-c-drawer > .pf-c-drawer__main > .pf-c-drawer__content > .pf-c-drawer__body
+ * This is a hack to get the drawer component inside the PageSectionComponent from the @kobsio/shared package working.
+ * When we are using the notification drawer component the drawer inside the PageSectionComponent doesn't use the full
+ * height anymore. With the following snippet this is fixed and the drawer is using the full height again.
+ * See also https://github.com/patternfly/patternfly-react/issues/7423 */
+.pf-c-drawer > .pf-c-drawer__main > .pf-c-drawer__content > .pf-c-drawer__body {
+  display: flex;
+  flex-direction: column;
+  flex-basis: 0%;
+  flex-grow: 1;
+  flex-shrink: 1;
+}