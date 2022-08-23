d58b6890543b789e623fb2ae1482c3b950b8e2db
ricoberger <mail@ricoberger.de>
2022-08-23 19:49:33 +0200

    [jira] Adjust notification style
    We are now using different styles for the issues displayed in the
    notification drawer. The style is based on the status of an issue. When
    an issue is in the status "Open" the notification item is yellow, for
    "In Progress" the notification item is blue and for "Done" is is green.
    For issues which are "Done" the notification is also marked as read.

--- plugins/plugin-jira/src/components/notifications/IssueNotification.tsx
+++ plugins/plugin-jira/src/components/notifications/IssueNotification.tsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { IPluginInstance, formatTime, pluginBasePath } from '@kobsio/shared';
 import { IIssue } from '../../utils/issue';
+import { getStatusVariant } from '../../utils/helpers';
 
 interface IIssueNotificationProps {
   instance: IPluginInstance;
@@ -20,15 +21,17 @@ const IssueNotification: React.FunctionComponent<IIssueNotificationProps> = ({
 }: IIssueNotificationProps) => {
   const navigate = useNavigate();
 
+  const variant = getStatusVariant(issue.fields?.status?.statusCategory.name);
+
   return (
     <NotificationDrawerListItem
-      variant="info"
-      isRead={false}
+      variant={variant}
+      isRead={variant === 'success' ? true : false}
       onClick={(): void =>
         navigate(`${pluginBasePath(instance)}/search?jql=${encodeURIComponent(`key = ${issue.key}`)}`)
       }
     >
-      <NotificationDrawerListItemHeader variant="info" title={issue.key || ''} />
+      <NotificationDrawerListItemHeader variant={variant} title={issue.key || ''} />
       <NotificationDrawerListItemBody
         timestamp={formatTime(Math.floor(new Date(issue.fields?.updated || '').getTime() / 1000))}
       >
