c46b4ff2dbdf1c15c2c4ee8daf1ab5232264d0a4
Robert Hecht <robert.hecht@staffbase.com>
2023-02-05 13:11:03 +0100

    refactor test
    - add json snapshot assertion
    - move huge json to file

--- pkg/utils/response.go
+++ pkg/utils/response.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"io"
+	"io/ioutil"
 	"net/http/httptest"
 	"testing"
 
@@ -23,4 +24,13 @@ func AssertJSONEq(t *testing.T, want string, r *httptest.ResponseRecorder) {
 	bytes, err := io.ReadAll(r.Body)
 	require.NoError(t, err)
 	require.JSONEq(t, want, string(bytes))
+}
+
+func AssertJSONSnapshotEq(t *testing.T, path string, r *httptest.ResponseRecorder) {
+	t.Helper()
+	bytes, err := io.ReadAll(r.Body)
+	require.NoError(t, err)
+	wantBytes, err := ioutil.ReadFile(path)
+	require.NoError(t, err)
+	require.JSONEq(t, string(wantBytes), string(bytes))
 }
