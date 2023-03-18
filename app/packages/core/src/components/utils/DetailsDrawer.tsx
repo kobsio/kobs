0bad9df453695c9f6fe5a7972f226be54fea071e
ricoberger <mail@ricoberger.de>
2023-03-18 12:58:39 +0100

    [core] Fix DetailsDrawer and Replace Paper Component
    This commit fixes the actions shown in the DetailsDrawer which were not
    aligned to the right side of the drawer. This is now fixed by adding the
    "flexGrow" property to the correct component.
    
    This commit also replaces the Paper component on the sign in page and
    the resources panel with a Card component, so that we do not have a
    bottom boarder, which looks a bit nicer.

--- app/packages/core/src/components/utils/DetailsDrawer.tsx
+++ app/packages/core/src/components/utils/DetailsDrawer.tsx
@@ -54,11 +54,12 @@ export const DetailsDrawer: FunctionComponent<IDetailsDrawerProps> = ({
               WebkitBoxOrient: 'vertical',
               WebkitLineClamp: '2',
               display: '-webkit-box',
+              flexGrow: 1,
               overflow: 'hidden',
               textOverflow: 'ellipsis',
             }}
           >
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" component="div">
               {title}
 
               {subtitle && (
