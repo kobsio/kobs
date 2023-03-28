66c5aa680c86a6d72f814f2e7052851e3d7a3cef
Robert Hecht <robert.hecht@staffbase.com>
2023-03-28 12:59:40 +0200

    clickhouse-driver

--- pkg/plugins/sql/instance/instance.go
+++ pkg/plugins/sql/instance/instance.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 	"math"
 
-	// _ "github.com/ClickHouse/clickhouse-go/v2"
+	_ "github.com/ClickHouse/clickhouse-go/v2"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	"github.com/mitchellh/mapstructure"
