743ed439683fd93ab85a8981f2afc655842e4e82
Rico Berger <ricoberger@users.noreply.github.com>
2023-05-20 23:26:10 +0200

    [core] Add "@codemirror/state" as External Module (#568)
    Add "@codemirror/state" as external module to the rollup configuration
    to not load multiple instances, which caused the following error:
    
      Unrecognized extension value in extension set ([object Object]).
      This sometimes happens because multiple instances of
      @codemirror/state are loaded, breaking instanceof checks.

--- app/packages/sql/vite.config.ts
+++ app/packages/sql/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     },
     rollupOptions: {
       external: [
+        '@codemirror/state',
         '@emotion/react',
         '@emotion/styled',
         '@kobsio/core',
