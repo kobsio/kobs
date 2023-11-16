081c3061bc37acbba98b95db9f96b4593060bc58
Thilo Schmalfuss <thilo.schmalfuss@gmail.com>
2023-11-16 11:23:24 +0100

    [tests] Improve sessions_test.go performance
    Use shared mongodb testcontainer.

--- pkg/hub/db/sessions_test.go
+++ pkg/hub/db/sessions_test.go
@@ -10,82 +10,77 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func TestCreateSession(t *testing.T) {
+func TestSessions(t *testing.T) {
 	uri, container := setupDatabase(t)
-	defer gnomock.Stop(container)
+	defer func(cs *gnomock.Container) {
+		err := gnomock.Stop(cs)
+		if err != nil {
+			t.Error(err)
+		}
+	}(container)
 	c, _ := NewClient(Config{URI: uri})
 
-	session, err := c.CreateSession(ctx(t), authContext.User{ID: "userid"})
-	require.NoError(t, err)
-	require.NotNil(t, session)
-}
-
-func TestGetSession(t *testing.T) {
-	uri, container := setupDatabase(t)
-	defer gnomock.Stop(container)
-	c, _ := NewClient(Config{URI: uri})
-
-	session, err := c.CreateSession(ctx(t), authContext.User{ID: "userid"})
-	require.NoError(t, err)
-	require.NotNil(t, session)
-
-	ctx := ctx(t)
-	t.Run("should return session", func(t *testing.T) {
-		actualSession, err := c.GetSession(ctx, session.ID)
+	t.Run("CreateSession", func(t *testing.T) {
+		session, err := c.CreateSession(ctx(t), authContext.User{ID: "userid"})
 		require.NoError(t, err)
-		require.Equal(t, session.ID, actualSession.ID)
-		require.Equal(t, session.User.ID, actualSession.User.ID)
+		require.NotNil(t, session)
 	})
 
-	t.Run("should fail to return session, when session does not exists", func(t *testing.T) {
-		_, err := c.GetSession(ctx, primitive.NewObjectID())
-		require.Error(t, err)
-		require.Equal(t, ErrSessionNotFound, err)
-	})
-}
-
-func TestGetAndUpdateSession(t *testing.T) {
-	uri, container := setupDatabase(t)
-	defer gnomock.Stop(container)
-	c, _ := NewClient(Config{URI: uri})
-
-	session, err := c.CreateSession(ctx(t), authContext.User{ID: "userid"})
-	require.NoError(t, err)
-	require.NotNil(t, session)
-
-	ctx := ctx(t)
-	t.Run("should return and update session", func(t *testing.T) {
-		actualSession, err := c.GetAndUpdateSession(ctx, session.ID)
+	t.Run("GetSession", func(t *testing.T) {
+		session, err := c.CreateSession(ctx(t), authContext.User{ID: "userid"})
 		require.NoError(t, err)
-		require.Equal(t, session.ID, actualSession.ID)
-		require.Equal(t, session.User.ID, actualSession.User.ID)
+		require.NotNil(t, session)
+
+		ctx := ctx(t)
+		t.Run("should return session", func(t *testing.T) {
+			actualSession, err := c.GetSession(ctx, session.ID)
+			require.NoError(t, err)
+			require.Equal(t, session.ID, actualSession.ID)
+			require.Equal(t, session.User.ID, actualSession.User.ID)
+		})
+
+		t.Run("should fail to return session, when session does not exists", func(t *testing.T) {
+			_, err := c.GetSession(ctx, primitive.NewObjectID())
+			require.Error(t, err)
+			require.Equal(t, ErrSessionNotFound, err)
+		})
 	})
 
-	t.Run("should fail to update session, when session does not exists", func(t *testing.T) {
-		_, err := c.GetAndUpdateSession(ctx, primitive.NewObjectID())
-		require.Error(t, err)
-		require.Equal(t, ErrSessionNotFound, err)
-	})
-}
-
-func TestDeleteSession(t *testing.T) {
-	uri, container := setupDatabase(t)
-	defer gnomock.Stop(container)
-	c, _ := NewClient(Config{URI: uri})
-
-	session, err := c.CreateSession(ctx(t), authContext.User{ID: "userid"})
-	require.NoError(t, err)
-	require.NotNil(t, session)
-
-	ctx := ctx(t)
-	t.Run("should delete session", func(t *testing.T) {
-		err := c.DeleteSession(ctx, session.ID)
+	t.Run("GetAndUpdateSession", func(t *testing.T) {
+		session, err := c.CreateSession(ctx(t), authContext.User{ID: "userid"})
 		require.NoError(t, err)
+		require.NotNil(t, session)
+
+		ctx := ctx(t)
+		t.Run("should return and update session", func(t *testing.T) {
+			actualSession, err := c.GetAndUpdateSession(ctx, session.ID)
+			require.NoError(t, err)
+			require.Equal(t, session.ID, actualSession.ID)
+			require.Equal(t, session.User.ID, actualSession.User.ID)
+		})
+
+		t.Run("should fail to update session, when session does not exists", func(t *testing.T) {
+			_, err := c.GetAndUpdateSession(ctx, primitive.NewObjectID())
+			require.Error(t, err)
+			require.Equal(t, ErrSessionNotFound, err)
+		})
 	})
 
-	t.Run("should fail to delete session, when session does not exists", func(t *testing.T) {
-		err := c.DeleteSession(ctx, primitive.NewObjectID())
-		require.Error(t, err)
-		require.Equal(t, ErrSessionNotFound, err)
+	t.Run("DeleteSession", func(t *testing.T) {
+		session, err := c.CreateSession(ctx(t), authContext.User{ID: "userid"})
+		require.NoError(t, err)
+		require.NotNil(t, session)
+
+		ctx := ctx(t)
+		t.Run("should delete session", func(t *testing.T) {
+			err := c.DeleteSession(ctx, session.ID)
+			require.NoError(t, err)
+		})
+
+		t.Run("should fail to delete session, when session does not exists", func(t *testing.T) {
+			err := c.DeleteSession(ctx, primitive.NewObjectID())
+			require.Error(t, err)
+			require.Equal(t, ErrSessionNotFound, err)
+		})
 	})
 }
